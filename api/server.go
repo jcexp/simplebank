00ed4d3cc9326700ef6fe85627a2bb974dab3f63
Jeff Chen <jeffcng@gmail.com>
2021-09-20 21:19:30 -0700

    server listAccount

--- api/server.go
+++ api/server.go
@@ -21,6 +21,7 @@ func NewServer(store *db.Store) *Server {
 	// TODO: add routes to router
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
 
 	server.router = router
 	return server
