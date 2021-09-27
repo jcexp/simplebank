d3fe8fa2d0a4d245a944a7fe2ec5f207f91b8569
Jeff Chen <jeffcng@gmail.com>
2021-09-26 21:45:52 -0700

    Add login API route to the server

--- api/server.go
+++ api/server.go
@@ -29,26 +29,34 @@ func NewServer(config util.Config, store db.Store) (*Server, error) {
 		store:      store,
 		tokenMaker: tokenMaker,
 	}
-	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	router.POST("/users", server.createUser)
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccount)
-	router.POST("/transfers", server.createTransfer)
-
-	server.router = router
+	server.setupRouter()
 	return server, nil
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run()
 }
 
 func errorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
 }
 
-func (server *Server) Start(address string) error {
-	return server.router.Run()
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
+
+	router.POST("/transfers", server.createTransfer)
+
+	server.router = router
 }
